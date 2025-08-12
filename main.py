1857fb399402340d7e365b00004704fbe674097d
Nattapol1994 <6522781994@g.siit.tu.ac.th>
2025-08-12 23:43:06 +0700

    Implemented the basics of InputHandler & refactor

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from src.Field import Field
 from src.Camera import Camera
+from src.InputHandler import InputHandler
 
 def main():
     pygame.init()
@@ -16,33 +17,12 @@ def main():
     clock = pygame.time.Clock()
 
     field = Field("test_field.tmj")
+    input_handler = InputHandler(camera)
 
     running = True
-    pan_speed = 10  # pixels to move per key press
-    zoom_speed = 0.1  # zoom increment per scroll
-    min_zoom = 0.5
-    max_zoom = 3.0
 
     while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            
-            elif event.type == pygame.MOUSEWHEEL:
-                # Zoom in/out with mouse wheel
-                camera.zoom += event.y * zoom_speed
-                camera.zoom = max(min_zoom, min(max_zoom, camera.zoom))
-        
-        # Key states for smooth panning
-        keys = pygame.key.get_pressed()
-        if keys[pygame.K_LEFT]:
-            camera.x -= pan_speed / camera.zoom  # divide by zoom for consistent speed
-        if keys[pygame.K_RIGHT]:
-            camera.x += pan_speed / camera.zoom
-        if keys[pygame.K_UP]:
-            camera.y -= pan_speed / camera.zoom
-        if keys[pygame.K_DOWN]:
-            camera.y += pan_speed / camera.zoom
+        running = input_handler.process_all_events()
 
         # Drawing
         screen.fill((30, 30, 30))
